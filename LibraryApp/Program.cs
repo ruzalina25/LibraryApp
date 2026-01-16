8c7d28b0fb25619f5bd2fed4fd17bcc748cf4254
Ruzalina <ruzalina13@icloud.com>
2026-01-16 22:24:55 +0500

    Lab 1: Created Book class with public fields and DisplayInfo method

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryApp.Models;
 
 namespace LibraryApp
 {
@@ -10,6 +11,13 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
+            Book myBook = new Book();
+            myBook.Title = "1984";
+            myBook.Author = "Джордж Оруэлл";
+            myBook.Year = 1949;
+            myBook.DisplayInfo();
+
+
         }
     }
 }
