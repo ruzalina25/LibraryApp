6785602d09f5de9afb63bf580858a98b3b6a2390
Ruzalina <ruzalina13@icloud.com>
2026-01-16 22:37:22 +0500

    Lab 2: Implemented encapsulation, properties, constructor with validation

--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -11,13 +11,28 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
-            Book myBook = new Book();
-            myBook.Title = "1984";
-            myBook.Author = "Джордж Оруэлл";
-            myBook.Year = 1949;
-            myBook.DisplayInfo();
+               try
+               {
+                 Book book1 = new Book("1984", "Дж. Оруэлл", 1949);
+                 Book book2 = new Book("Гарри Поттер", "Дж. Роулинг", 1997);
+                 Book book3 = new Book("Мастер и Маргарита", "М. Булгаков", 1967);
+
+                book1.DisplayInfo();
+                book2.DisplayInfo();
+                book3.DisplayInfo();
+
+                
+                Book bookError = new Book("Неизвестная книга", "неизвестно", -5);
+                bookError.DisplayInfo();
+                  
+               }
+              catch (Exception ex)
+               {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+               }
 
 
+
+            }
         }
-    }
 }
