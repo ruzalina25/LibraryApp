7682e80e7cd9c7ffc2166f5df559b67a1bf30b55
Ruzalina <ruzalina13@icloud.com>
2026-01-16 22:57:26 +0500

    Lab 3: Implemented inheritance with LibraryItem, Book, and Magazine

---
+++ LibraryApp/Models/LibraryItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryApp.Models;
+
+namespace LibraryApp.Models
+{
+    public abstract class LibraryItem
+    {
+        public string Title { get; protected set; }
+        public string Author { get; protected set; }
+        public int Year { get; protected set; }
+        protected LibraryItem(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public abstract void DisplayInfo();
+
+    
+
+
+
+    }
+}
--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -5,34 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using LibraryApp.Models;
 
+
+
 namespace LibraryApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-               try
-               {
-                 Book book1 = new Book("1984", "Дж. Оруэлл", 1949);
-                 Book book2 = new Book("Гарри Поттер", "Дж. Роулинг", 1997);
-                 Book book3 = new Book("Мастер и Маргарита", "М. Булгаков", 1967);
 
-                book1.DisplayInfo();
-                book2.DisplayInfo();
-                book3.DisplayInfo();
+            var items = new List<LibraryItem>
+             {
+                     new Book("1984", "Оруэлл", 1949, 328),
+                     new Magazine("Science", "Редколлегия", 2023, 5),
+                    new Book("Анна Каренина", "Толстой", 1877, 850)
+             };
 
-                
-                Book bookError = new Book("Неизвестная книга", "неизвестно", -5);
-                bookError.DisplayInfo();
-                  
-               }
-              catch (Exception ex)
-               {
-                    Console.WriteLine($"Ошибка: {ex.Message}");
-               }
-
-
-
+            foreach (var item in items)
+            {
+                item.DisplayInfo();
             }
+
+            /* try
+             {
+                  Book book1 = new Book("1984", "Дж. Оруэлл", 1949);
+                  Book book2 = new Book("Гарри Поттер", "Дж. Роулинг", 1997);
+                  Book book3 = new Book("Мастер и Маргарита", "М. Булгаков", 1967);
+
+                 book1.DisplayInfo();
+                 book2.DisplayInfo();
+                 book3.DisplayInfo();
+
+
+                 Book bookError = new Book("Неизвестная книга", "неизвестно", -5);
+                 bookError.DisplayInfo();
+
+                }
+               catch (Exception ex)
+                {
+                     Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+
+
+
+             }*/
         }
+    }
 }
